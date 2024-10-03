48348e0000bee7defcc78a8cab4632221d8ffc23
Farah Guelbi <farah33guelbi@gmail.com>
2024-10-03 22:53:51 +0100

    Atelier2

---
+++ src/app/app.module.ts
@@ -0,0 +1,29 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BindingComponent } from './binding/binding.component';
+import { ProduitsComponent } from './produits/produits.component';
+import { AddProduitComponent } from './add-produit/add-produit.component';
+import { UpdateProduitComponent } from './update-produit/update-produit.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    BindingComponent,
+    ProduitsComponent,
+    AddProduitComponent,
+    UpdateProduitComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule
+  ],
+  providers: [
+    provideClientHydration()
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
